e5395dcd2ecbc6e6debc6507b076e12824238758
TK <caiotkfukuda@gmail.com>
2024-08-25 15:00:35 -0300

    Codigo Terminal de banco

---
+++ DIO/BancoTerminal.java
@@ -0,0 +1,13 @@
+
+public class BancoTerminal{
+        public static void main(String[] args) {
+            double saldo = 15.0;
+            double valorSolicitado = 18.0;
+             if (saldo >= valorSolicitado){
+                saldo = saldo - valorSolicitado;
+                System.out.printf("Saque realizado com sucesso. Saldo: R$ %.2f%n", saldo);
+             }else {
+                System.out.println("Saldo insuficiente.");
+             }
+    }
+}
