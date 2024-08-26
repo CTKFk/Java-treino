43b796e26d3231f5d1666037b3a29ab8c58921cf
TK <caiotkfukuda@gmail.com>
2024-08-26 14:15:21 -0300

    2

--- DIO/BancoTerminal.java
+++ DIO/BancoTerminal.java
@@ -1,8 +1,7 @@
-
 public class BancoTerminal{
         public static void main(String[] args) {
-            double saldo = 15.0;
-            double valorSolicitado = 18.0;
+            double saldo = 25.0;
+            double valorSolicitado = 22.0;
              if (saldo >= valorSolicitado){
                 saldo = saldo - valorSolicitado;
                 System.out.printf("Saque realizado com sucesso. Saldo: R$ %.2f%n", saldo);
