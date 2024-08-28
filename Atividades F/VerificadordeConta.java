78774a7041e771b85220d9600cb374e57ffdec03
CTKFk <Caiotkfukuda@gmail.com>
2024-08-28 15:42:37 -0300

    Update VerificadordeConta.java

--- Atividades F/VerificadordeConta.java
+++ Atividades F/VerificadordeConta.java
@@ -6,6 +6,9 @@ public class VerificadordeConta {
         try (Scanner nconta = new Scanner(System.in)) {
             System.out.print("Digite o número da conta (até 6 dígitos): ");
             String numeroConta = nconta.nextLine();
+            if (numeroConta.length() > 6 ) {
+             System.out.printf("Número de conta invalido");   
+          } else {    
             
             int soma = 0;
             for (int i = 0; i < numeroConta.length(); i++) {
@@ -19,3 +22,21 @@ public class VerificadordeConta {
         }
     }
 }
+/*
+Alternativa com int e restrição de numero
+ String numero;
+  int s;
+  int digito;
+  
+  System.out.print("Digite o número da conta (até 6 dígitos): ");
+  numero = input.nextLine();     
+  if (numero.length() > 6 ) {
+     System.out.printf("Número de conta invalido");   
+  } else {    
+     s = 0;
+     for (int i = 0; i < numero.length(); i++) {
+        s += Character.getNumericValue(numero.charAt(i));
+     }
+     digito = s % 10;
+     System.out.printf("O número da conta é: %06d-%d%n", Integer.valueOf(numero), digito);
+  }*/
