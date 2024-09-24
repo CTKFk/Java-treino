833c5628853d54a295e3ffb9fd5a057a293b1b2a
expur <leotokuo@gmail.com>
2024-09-24 15:35:54 -0300

    Create TesteRetangulo.java
    Meu nome é Yoshikage Kira. Tenho 33 anos. Minha casa fica na parte nordeste de Morioh, onde todas as casas estão, e eu não sou casado. Eu trabalho como funcionário das lojas de departamentos Kame Yu e chego em casa todos os dias às oito da noite, no máximo. Eu não fumo, mas ocasionalmente bebo. Estou na cama às 23 horas e me certifico de ter oito horas de sono, não importa o que aconteça. Depois de tomar um copo de leite morno e fazer cerca de vinte minutos de alongamentos antes de ir para a cama, geralmente não tenho problemas para dormir até de manhã. Assim como um bebê, eu acordo sem nenhum cansaço ou estresse pela manhã. Foi-me dito que não houve problemas no meu último check-up. Estou tentando explicar que sou uma pessoa que deseja viver uma vida muito tranquila. Eu cuido para não me incomodar com inimigos, como ganhar e perder, isso me faria perder o sono à noite. É assim que eu lido com a sociedade e sei que é isso que me traz felicidade. Embora, se eu fosse lutar, não perderia para ninguém.

---
+++ atividades.f/TesteRetangulo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TesteRetangulo
+{
+   public static void main(String[] args) 
+   {
+      Retangulo ret1 = new Retangulo();
+        ret1.exibeDados();
+
+      Retangulo ret2 = new Retangulo(6, 5);
+        ret2.exibeDados();
+   }
+
+}
